3840ef2e2607d5837d4f67a24e0d72f2ed588391
Rovx <ericdotesi@gmail.com>
2025-01-29 20:33:49 +0100

    Solved redirections

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,10 +1,15 @@
 <?php
 
-function routeWithPreview($name, $parameters = [])
-{
-    $route = route($name, $parameters);
-    if (env('MAINTENANCE_MODE') === 'true') {
-        return $route . '?preview=' . env('MAINTENANCE_TOKEN');
+if (!function_exists('routeWithPreview')) {
+    function routeWithPreview($name, $parameters = []) {
+        $route = route($name, $parameters);
+
+        // Si estamos en modo mantenimiento O ya hay un token válido en la URL actual
+        if (env('MAINTENANCE_MODE') === 'true' ||
+            request()->query('preview') === env('MAINTENANCE_TOKEN')) {
+            return $route . '?preview=' . env('MAINTENANCE_TOKEN');
+        }
+
+        return $route;
     }
-    return $route;
 }
