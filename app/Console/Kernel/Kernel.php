460fb3035e7c29342017da1b54344cfa468814ac
Rovx <ericdotesi@gmail.com>
2025-02-06 18:20:22 +0100

    updated cron job

--- app/Console/Kernel/Kernel.php
+++ app/Console/Kernel/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('gifts:clear-expired')
-            ->hourly()
+            ->everyMinute()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/scheduler.log'));
     }
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
