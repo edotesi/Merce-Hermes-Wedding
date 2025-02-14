887d16acfc15c0de14741547e5159fb334b23821
Rovx <ericdotesi@gmail.com>
2025-02-14 14:41:33 +0100

    applied new photo and more styles

--- database/seeders/GiftSeeder.php
+++ database/seeders/GiftSeeder.php
@@ -13,7 +13,7 @@ class GiftSeeder extends Seeder
             [
                 'name' => 'Aspiradora sin cables Dyson Gen5detect™ Absolute',
                 'price' => 809.00,
-                'image_url' => 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/products/cord-free-vacuums/sticks/Dyson-Gen5detect/shop-all/418-Web-shop-all-Choose-your-Dyson-Gen5Detect-PrussBlue-Copper.jpg?$responsive$&cropPathE=mobile&fit=stretch,1&fmt=pjpeg&wid=640',
+                'image_url' => 'https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/products/floorcare/sticks/418/pdp/iron-purple/418-PDP-Video-Thumbnail-Purple.jpg?$responsive$&cropPathE=desktop&fit=stretch,1&wid=1920',
                 'product_url' => 'https://www.dyson.es/aspiradoras/sin-cable/gen5detect/absolute-niquel-morado',
                 'stock' => 1
             ],
@@ -27,7 +27,7 @@ class GiftSeeder extends Seeder
             [
                 'name' => 'Lattissima One Porcelain White',
                 'price' => 289.90,
-                'image_url' => '/storage/gifts/roomba.jpg',
+                'image_url' => 'https://cb.scene7.com/is/image/Crate/Lttssma1EVODlngSWhtSSS21_VND',
                 'product_url' => 'https://www.nespresso.com/ecom/medias/sys_master/public/31372890439710/Global-All-OL-Lattissima-One-White-Front-View-Horizontale-Without-Caps-square-2021-2026.jpg?impolicy=medium&imwidth=775',
                 'stock' => 1
             ],
